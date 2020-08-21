5d71156f1043b0ae3cb029dc52f86d48b70b53a0
nuintun <nuintun@qq.com>
2020-08-21 15:12:44 +0800

    :arrow_up: Bump deps

--- URI.js
+++ URI.js
@@ -8,10 +8,13 @@
  */
 
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-  typeof define === 'function' && define.amd ? define(factory) :
-  (global = global || self, global.URI = factory());
-}(this, (function () { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined'
+    ? (module.exports = factory())
+    : typeof define === 'function' && define.amd
+    ? define(factory)
+    : ((global = typeof globalThis !== 'undefined' ? globalThis : global || self), (global.URI = factory()));
+})(this, function () {
+  'use strict';
 
   /**
    * @module URI
@@ -33,9 +36,8 @@
    * @returns {T|null}
    */
   function normalize(value) {
-      if (value == null)
-          return null;
-      return value;
+    if (value == null) return null;
+    return value;
   }
   /**
    * @function isNotNullAndUndef
@@ -43,7 +45,7 @@
    * @returns {boolean}
    */
   function isNotNullAndUndef(value) {
-      return value != null;
+    return value != null;
   }
   /**
    * @function encode
@@ -51,9 +53,8 @@
    * @returns {string|null}
    */
   function encode(value) {
-      if (!value)
-          return value;
-      return encodeURIComponent(value);
+    if (!value) return value;
+    return encodeURIComponent(value);
   }
   /**
    * @function decode
@@ -61,9 +62,8 @@
    * @returns {string|null}
    */
   function decode(value) {
-      if (!value)
-          return value;
-      return decodeURIComponent(value);
+    if (!value) return value;
+    return decodeURIComponent(value);
   }
   /**
    * @function parse
@@ -71,32 +71,29 @@
    * @returns {{[key:string]:any}}
    */
   function parse(search) {
-      var query = {};
-      if (!search)
-          return query;
-      search = search.replace(/^[?#]/, '');
-      if (search) {
-          while (true) {
-              var matched = PARSE_QUERY_REGEX.exec(search);
-              if (matched) {
-                  var key = decode(matched[1] || '');
-                  var value = decode(normalize(matched[2]));
-                  if (query.hasOwnProperty(key)) {
-                      if (!Array.isArray(query[key])) {
-                          query[key] = [query[key]];
-                      }
-                      query[key].push(value);
-                  }
-                  else {
-                      query[key] = value;
-                  }
-              }
-              else {
-                  break;
-              }
+    var query = {};
+    if (!search) return query;
+    search = search.replace(/^[?#]/, '');
+    if (search) {
+      while (true) {
+        var matched = PARSE_QUERY_REGEX.exec(search);
+        if (matched) {
+          var key = decode(matched[1] || '');
+          var value = decode(normalize(matched[2]));
+          if (query.hasOwnProperty(key)) {
+            if (!Array.isArray(query[key])) {
+              query[key] = [query[key]];
+            }
+            query[key].push(value);
+          } else {
+            query[key] = value;
           }
+        } else {
+          break;
+        }
       }
-      return query;
+    }
+    return query;
   }
   /**
    * @function stringify
@@ -105,130 +102,134 @@
    * @returns {string}
    */
   function stringify(query, prefix) {
-      var search = '';
-      var _loop_1 = function (key) {
-          if (query.hasOwnProperty(key)) {
-              var value = query[key];
-              // Encode key
-              key = encode(key);
-              if (Array.isArray(value)) {
-                  value.forEach(function (item) {
-                      search += '&' + key;
-                      if (isNotNullAndUndef(item)) {
-                          search += '=' + encode(item);
-                      }
-                  });
-              }
-              else {
-                  search += '&' + key;
-                  if (isNotNullAndUndef(value)) {
-                      search += '=' + encode(value);
-                  }
-              }
+    var search = '';
+    var _loop_1 = function (key) {
+      if (query.hasOwnProperty(key)) {
+        var value = query[key];
+        // Encode key
+        key = encode(key);
+        if (Array.isArray(value)) {
+          value.forEach(function (item) {
+            search += '&' + key;
+            if (isNotNullAndUndef(item)) {
+              search += '=' + encode(item);
+            }
+          });
+        } else {
+          search += '&' + key;
+          if (isNotNullAndUndef(value)) {
+            search += '=' + encode(value);
           }
-      };
-      for (var key in query) {
-          _loop_1(key);
+        }
       }
-      return search.replace(/^&/, prefix);
+    };
+    for (var key in query) {
+      _loop_1(key);
+    }
+    return search.replace(/^&/, prefix);
   }
   /**
    * @class URI
    */
   var URI = /** @class */ (function () {
-      /**
-       * @constructor
-       * @param {string} URI
-       */
-      function URI(URI) {
-          var context = this;
-          var matched = WHATWG_URI_REGEX.exec(URI);
-          // Normalize URI
-          if (!matched) {
-              throw Error('URI not a standard WHATWG URI.');
-          }
-          var 
-          // Matched
-          protocol = matched[1], username = matched[2], password = matched[3], hostname = matched[4], port = matched[5], pathname = matched[6], search = matched[7], hash = matched[8];
-          context.protocol = normalize(protocol);
-          context.username = normalize(username);
-          context.password = normalize(password);
-          context.hostname = normalize(hostname);
-          context.port = normalize(port);
-          context.pathname = normalize(pathname);
-          context.query = parse(search);
-          context.fragment = parse(hash);
+    /**
+     * @constructor
+     * @param {string} URI
+     */
+    function URI(URI) {
+      var context = this;
+      var matched = WHATWG_URI_REGEX.exec(URI);
+      // Normalize URI
+      if (!matched) {
+        throw Error('URI not a standard WHATWG URI.');
       }
-      Object.defineProperty(URI.prototype, "search", {
-          /**
-           * @property search
-           * @method get
-           * @returns {string}
-           */
-          get: function () {
-              return stringify(this.query, '?');
-          },
-          enumerable: false,
-          configurable: true
-      });
-      Object.defineProperty(URI.prototype, "hash", {
-          /**
-           * @property hash
-           * @method get
-           * @returns {string}
-           */
-          get: function () {
-              return stringify(this.fragment, '#');
-          },
-          enumerable: false,
-          configurable: true
-      });
+      var // Matched
+        protocol = matched[1],
+        username = matched[2],
+        password = matched[3],
+        hostname = matched[4],
+        port = matched[5],
+        pathname = matched[6],
+        search = matched[7],
+        hash = matched[8];
+      context.protocol = normalize(protocol);
+      context.username = normalize(username);
+      context.password = normalize(password);
+      context.hostname = normalize(hostname);
+      context.port = normalize(port);
+      context.pathname = normalize(pathname);
+      context.query = parse(search);
+      context.fragment = parse(hash);
+    }
+    Object.defineProperty(URI.prototype, 'search', {
       /**
-       * @method toURI
+       * @property search
+       * @method get
        * @returns {string}
        */
-      URI.prototype.toURI = function () {
-          var URI = '';
-          var context = this;
-          var protocol = context.protocol;
-          var username = context.username;
-          var password = context.password;
-          var hostname = context.hostname;
-          var port = context.port;
-          if (isNotNullAndUndef(protocol)) {
-              URI += protocol;
-          }
-          if (isNotNullAndUndef(protocol)) {
-              URI += '//';
-          }
-          if (isNotNullAndUndef(username)) {
-              URI += username;
-          }
-          if (isNotNullAndUndef(password)) {
-              URI += ':' + password;
-          }
-          if (isNotNullAndUndef(username) || isNotNullAndUndef(password)) {
-              URI += '@';
-          }
-          if (isNotNullAndUndef(hostname)) {
-              URI += hostname;
-          }
-          if (isNotNullAndUndef(port)) {
-              URI += ':' + port;
-          }
-          URI += context.pathname + context.search + context.hash;
-          return URI;
-      };
+      get: function () {
+        return stringify(this.query, '?');
+      },
+      enumerable: false,
+      configurable: true
+    });
+    Object.defineProperty(URI.prototype, 'hash', {
       /**
-       * @method toString
+       * @property hash
+       * @method get
        * @returns {string}
        */
-      URI.prototype.toString = function () {
-          return this.toURI();
-      };
+      get: function () {
+        return stringify(this.fragment, '#');
+      },
+      enumerable: false,
+      configurable: true
+    });
+    /**
+     * @method toURI
+     * @returns {string}
+     */
+    URI.prototype.toURI = function () {
+      var URI = '';
+      var context = this;
+      var protocol = context.protocol;
+      var username = context.username;
+      var password = context.password;
+      var hostname = context.hostname;
+      var port = context.port;
+      if (isNotNullAndUndef(protocol)) {
+        URI += protocol;
+      }
+      if (isNotNullAndUndef(protocol)) {
+        URI += '//';
+      }
+      if (isNotNullAndUndef(username)) {
+        URI += username;
+      }
+      if (isNotNullAndUndef(password)) {
+        URI += ':' + password;
+      }
+      if (isNotNullAndUndef(username) || isNotNullAndUndef(password)) {
+        URI += '@';
+      }
+      if (isNotNullAndUndef(hostname)) {
+        URI += hostname;
+      }
+      if (isNotNullAndUndef(port)) {
+        URI += ':' + port;
+      }
+      URI += context.pathname + context.search + context.hash;
       return URI;
-  }());
+    };
+    /**
+     * @method toString
+     * @returns {string}
+     */
+    URI.prototype.toString = function () {
+      return this.toURI();
+    };
+    return URI;
+  })();
 
   return URI;
-
-})));
+});
